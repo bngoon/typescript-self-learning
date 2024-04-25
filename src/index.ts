e3b9a80cc5fc40e23e40998fe791eadd7ad3265d
Booker Ngoon <bookerngoon@gmail.com>
2024-04-25 12:56:37 -0400

    type alias

--- src/index.ts
+++ src/index.ts
@@ -2,25 +2,23 @@ console.log("hello world");
 
 let age: number = 20;
 
-if (age < 50)
-    age += 10;
+if (age < 50) age += 10;
 console.log(age);
 
 let sales: number = 123_456_789;
-let course: string = 'TypeScript';
+let course: string = "TypeScript";
 let is_published: boolean = true;
 
 function render(document: any) {
-    console.log(document)
+  console.log(document);
 }
 
 let numbers: number[] = [1, 2, 3];
 
-numbers.forEach(n => n.toString)
+numbers.forEach((n) => n.toString);
 
-
-// tuples 
-let user: [number, string] = [1, "Booker"]
+// tuples
+let user: [number, string] = [1, "Booker"];
 user.push(1);
 
 // enums
@@ -29,28 +27,35 @@ const medium = 2;
 const large = 3;
 
 //pascal naming convention
-const enum Size {Small = 1, Medium , Large};
+const enum Size {
+  Small = 1,
+  Medium,
+  Large,
+}
 let mySize: Size = Size.Medium;
 console.log(mySize);
 
 function calculateTax(income: number, taxYear: number): number {
-    if (taxYear || 2022 < 2022)
-        return income * 1.2;
-    return income * 1.3;
-
+  if (taxYear || 2022 < 2022) return income * 1.2;
+  return income * 1.3;
 }
-calculateTax(10_000, 2022)
+calculateTax(10_000, 2022);
 
 
-let employee: {
-    readonly id: number,
-    name: string,
-    retire: (date: Date) => void
-} = {
-     id: 1,
-      name: 'Booker',
-      retire: (date: Date) => {
-        console.log(date);
-      }
-     };
+
+
+type Employee = {
+    readonly id: number;
+    name: string;
+    retire: (date: Date) => void;
+}
+
+
+let employee: Employee = {
+  id: 1,
+  name: "Booker",
+  retire: (date: Date) => {
+    console.log(date);
+  },
+};
 
