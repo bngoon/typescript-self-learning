84754590e1faa265b1c97242b5d09353604a5477
Booker Ngoon <bookerngoon@gmail.com>
2024-04-25 17:24:25 -0400

    optional chaining

--- dist/index.js
+++ dist/index.js
@@ -46,8 +46,16 @@ let textBox = {
 };
 let quantity = 100;
 function greet(name) {
-    console.log(name.toLocaleUpperCase());
+    if (name)
+        console.log(name.toLocaleUpperCase());
+    else
+        console.log('Hola!');
 }
 ;
 greet('Booker');
+function getCustomer(id) {
+    return id === 0 ? null : { birthday: new Date() };
+}
+let customer = getCustomer(1);
+console.log(customer === null || customer === void 0 ? void 0 : customer.birthday);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -102,3 +102,21 @@ else
 console.log('Hola!')
 };
 greet('Booker');
+
+//optional chaining
+type Customer = {
+  birthday: Date
+};
+
+function getCustomer(id: number): Customer | null {
+  return id === 0 ? null : {birthday: new Date()};
+}
+
+
+let customer = getCustomer(1);
+//optional property access operator
+
+  console.log(customer?.birthday?.getFullYear());
+
+
+  
