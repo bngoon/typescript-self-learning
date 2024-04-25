2d5aeabf4705cf746d74a891aac168c571f07bae
Booker Ngoon <bookerngoon@gmail.com>
2024-04-25 12:28:58 -0400

    functions

--- dist/index.js
+++ dist/index.js
@@ -20,4 +20,10 @@ const large = 3;
 ;
 let mySize = 2;
 console.log(mySize);
+function calculateTax(income, taxYear) {
+    if (taxYear < 2022)
+        return income * 1.2;
+    return income * 1.3;
+}
+calculateTax(10000, 2022);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -32,3 +32,13 @@ const large = 3;
 const enum Size {Small = 1, Medium , Large};
 let mySize: Size = Size.Medium;
 console.log(mySize);
+
+function calculateTax(income: number, taxYear: number): number {
+    if (taxYear || 2022 < 2022)
+        return income * 1.2;
+    return income * 1.3;
+
+}
+calculateTax(10_000, 2022)
+
+
