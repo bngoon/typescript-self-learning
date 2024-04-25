fdb082cf3ff758aa6e5a49408b845155c763ad80
Booker Ngoon <bookerngoon@gmail.com>
2024-04-25 16:18:24 -0400

    union types

--- dist/index.js
+++ dist/index.js
@@ -5,19 +5,18 @@ if (age < 50)
     age += 10;
 console.log(age);
 let sales = 123456789;
-let course = 'TypeScript';
+let course = "TypeScript";
 let is_published = true;
 function render(document) {
     console.log(document);
 }
 let numbers = [1, 2, 3];
-numbers.forEach(n => n.toString);
+numbers.forEach((n) => n.toString);
 let user = [1, "Booker"];
 user.push(1);
 const small = 1;
 const medium = 2;
 const large = 3;
-;
 let mySize = 2;
 console.log(mySize);
 function calculateTax(income, taxYear) {
@@ -28,9 +27,17 @@ function calculateTax(income, taxYear) {
 calculateTax(10000, 2022);
 let employee = {
     id: 1,
-    name: 'Booker',
+    name: "Booker",
     retire: (date) => {
         console.log(date);
-    }
+    },
 };
+function kgToLbs(weight) {
+    if (typeof weight === 'number')
+        return weight * 2.2;
+    else
+        return parseInt(weight) * 2.2;
+}
+kgToLbs(10);
+kgToLbs('10kg');
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -59,3 +59,15 @@ let employee: Employee = {
   },
 };
 
+function kgToLbs(weight: number | string): number {
+  //narrowing
+  if (typeof weight === 'number') 
+    return weight * 2.2;
+  else
+  return parseInt(weight) * 2.2
+  
+}
+
+kgToLbs(10);
+kgToLbs('10kg');
+
