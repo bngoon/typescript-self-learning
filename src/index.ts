5ae224799434a868335511fcab45514d44283bf6
Booker Ngoon <bookerngoon@gmail.com>
2024-04-25 16:20:51 -0400

    type intersection

--- src/index.ts
+++ src/index.ts
@@ -71,3 +71,19 @@ function kgToLbs(weight: number | string): number {
 kgToLbs(10);
 kgToLbs('10kg');
 
+type Draggable = {
+  drag: () => void
+};
+
+type Resizable = {
+  resize: () => void
+};
+
+type UIWidget = Draggable & Resizable;
+
+let textBox: UIWidget = {
+  drag: () => {},
+  resize: () => {}
+
+}
+
